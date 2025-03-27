2545695d32d4c5bc839f5d684f28447a4463ae28
yuurekuji <Kanoyxng@gmail.com>
2025-03-27 09:51:39 -0400

    Added button for the character attack

---
+++ Assets/Week 10/Attack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void attack()
+    {
+        animator.Play("HeroKnight_Attack1");
+    }
+}
