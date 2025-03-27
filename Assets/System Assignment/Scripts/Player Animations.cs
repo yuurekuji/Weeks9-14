fd6dbca5291fed941473ebcf2a71f29d89b1da67
yuurekuji <Kanoyxng@gmail.com>
2025-03-27 11:41:41 -0400

    Added buttons and animations for the player during combat, when the player in in combat start music. Asset pack for music has also been added

---
+++ Assets/System Assignment/Scripts/Player Animations.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void attack()
+    {
+        animator.Play("Attack1"); // this plays the animation for the attack.
+    }
+
+    public void Dodge()
+    {
+        animator.Play("Perfect Dodge"); // this plays the animation for the perfect dodge / parry.
+    }
+}
